e3803ec4800627ef5c8cc15862e717995274a585
Pavel Tupitsyn <ptupitsyn@apache.org>
2022-05-19 14:42:44 +0300

    wip ClientSession, ClientSessionHandler

---
+++ modules/client-handler/src/main/java/org/apache/ignite/client/handler/ClientSession.java
@@ -0,0 +1,16 @@
+package org.apache.ignite.client.handler;
+
+import java.util.UUID;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public final class ClientSession {
+    /** Session ID. */
+    private final UUID id = UUID.randomUUID();
+
+    /** Session resources. */
+    private final ClientResourceRegistry resources = new ClientResourceRegistry();
+
+    /** Pending responses (TODO data structure choice - ?). */
+    private final ConcurrentLinkedQueue<Object> responseQueue = new ConcurrentLinkedQueue<>();
+
+}
---
+++ modules/client-handler/src/main/java/org/apache/ignite/client/handler/ClientSessionHandler.java
@@ -0,0 +1,4 @@
+package org.apache.ignite.client.handler;
+
+public final class ClientSessionHandler {
+}
