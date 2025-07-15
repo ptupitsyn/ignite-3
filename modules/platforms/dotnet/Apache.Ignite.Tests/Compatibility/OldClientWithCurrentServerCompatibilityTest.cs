b98ea8619a6e694e205e54deddaaeea229c8405a
Pavel Tupitsyn <ptupitsyn@apache.org>
2025-07-15 12:38:42 +0300

    Add NuGet library

--- modules/platforms/dotnet/Apache.Ignite.Tests/Compatibility/OldClientWithCurrentServerCompatibilityTest.cs
+++ modules/platforms/dotnet/Apache.Ignite.Tests/Compatibility/OldClientWithCurrentServerCompatibilityTest.cs
@@ -17,7 +17,15 @@
 
 namespace Apache.Ignite.Tests.Compatibility;
 
+using System;
+using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
+using NuGet.Common;
+using NuGet.Packaging;
+using NuGet.Protocol;
+using NuGet.Protocol.Core.Types;
+using NuGet.Versioning;
 using NUnit.Framework;
 
 /// <summary>
@@ -34,6 +42,28 @@ public class OldClientWithCurrentServerCompatibilityTest : IgniteTestsBase
     [Test]
     public async Task TestDownloadNuGetPackage()
     {
-        // TODO: Use nuget library.
+        SourceCacheContext cache = new SourceCacheContext();
+        SourceRepository repository = Repository.Factory.GetCoreV3("https://api.nuget.org/v3/index.json");
+        FindPackageByIdResource resource = await repository.GetResourceAsync<FindPackageByIdResource>();
+
+        string packageId = "Apache.Ignite";
+        NuGetVersion packageVersion = new NuGetVersion(_clientVersion);
+        using MemoryStream packageStream = new MemoryStream();
+
+        await resource.CopyNupkgToStreamAsync(
+            packageId,
+            packageVersion,
+            packageStream,
+            cache,
+            NullLogger.Instance,
+            CancellationToken.None);
+
+        Console.WriteLine($"Downloaded package {packageId} {packageVersion}");
+
+        using PackageArchiveReader packageReader = new PackageArchiveReader(packageStream);
+        NuspecReader nuspecReader = await packageReader.GetNuspecReaderAsync(CancellationToken.None);
+
+        Console.WriteLine($"Tags: {nuspecReader.GetTags()}");
+        Console.WriteLine($"Description: {nuspecReader.GetDescription()}");
     }
 }
