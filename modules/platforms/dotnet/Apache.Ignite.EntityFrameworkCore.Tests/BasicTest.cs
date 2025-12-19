a058cafdb0cc19bb621fdfbc97e0de9a8f17495e
Pavel Tupitsyn <ptupitsyn@apache.org>
2025-12-19 14:32:31 +0200

    wip

--- modules/platforms/dotnet/Apache.Ignite.EntityFrameworkCore.Tests/BasicTest.cs
+++ modules/platforms/dotnet/Apache.Ignite.EntityFrameworkCore.Tests/BasicTest.cs
@@ -96,7 +96,7 @@ public class BasicTest
         using var client = await GetClient();
         var tables = await client.Tables.GetTablesAsync();
 
-        // TODO: "PUBLIC."__EFMigrationsHistory"" name is wrong, we prepend schema icorrectly somewhere.
+        // TODO: Re-test "dotnet ef migrations add" and "dotnet ef database update" commands once migrations are supported.
         CollectionAssert.AreEquivalent(
             new[] { "Authors", "Books", "__EFMigrationsHistory" },
             tables.Select(t => t.Name));
